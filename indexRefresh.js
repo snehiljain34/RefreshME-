fc148df28a1e710f468c32e9d0101c02d45a1405
snehiljain34 <54562587+snehiljain34@users.noreply.github.com>
2020-06-30 18:49:38 +0530

    Update indexRefresh.js

--- indexRefresh.js
+++ indexRefresh.js
@@ -1,7 +1,7 @@
-alert("Refresh the page to get the dice rolling!");
+
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".PNG";
-document.querySelectorAll("img")[0].setAttribute("src", randomeDiceImage);
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 
 
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
