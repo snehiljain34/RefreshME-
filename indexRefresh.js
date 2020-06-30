e792d4e3f8d5f4781849dff576b850802e172b41
snehiljain34 <54562587+snehiljain34@users.noreply.github.com>
2020-06-30 17:34:24 +0530

    Update indexRefresh.js

--- indexRefresh.js
+++ indexRefresh.js
@@ -1,13 +1,10 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".PNG";
-var randomeImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomeImageSource);
+image1.setAttribute("src", randomeDiceImage);
 
 
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".PNG";
+var randomImageSource2 = "dice" + randomNumber2 + ".PNG";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1>randomNumber2){
